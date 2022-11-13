24ef27c19b233fabe76c641624ffd8839ac5331a
IdoiaVelez <ivelez34@gmail.com>
2022-11-13 09:07:40 +0100

    Condicionales Switch

--- src/App.java
+++ src/App.java
@@ -5,30 +5,18 @@ public class App {
         // System.out.println("Básico, pero importante");
 
         /* if */
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Introduce tu edad: ");
-            int age = scanner.nextInt();
-
-            if (age > 0) {
-                if (age < 18) {
-                    System.out.println("Too young");
-                } else if (age > 100) {
-                    System.out.println("Really?");
-                } else {
-                    System.out.println("Good!");
-                }
-            }
-
-        }
+        
+         /* s */
+        
 
         /* switch */
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Introduce un número del 1 al 7");
+            System.out.print("Introduce un número del 1 al 7: ");
             int day = scanner.nextInt();
 
             switch (day) {
                 case 1:
-                    System.out.println("Lunes")
+                    System.out.println("Lunes");
                     break;
                 case 2:
                     System.out.println("Martes");
@@ -37,21 +25,22 @@ public class App {
                     System.out.println("Miércoles");
                     break;
                 case 4:
-                    System.out.println("Jueves";
+                    System.out.println("Jueves");
                     break;
                 case 5:
                     System.out.println("Viernes");
                     break;
                 case 6:
                     System.out.println("Sábado");
-                    breack;
+                    break;
                 case 7:
                     System.out.println("Dominto");
-                    breack;
+                    break;
                 default:
-                    System.out.println("Un número del 1 al 7, por favor.")
-                    )
+                    System.out.println("Un número del 1 al 7, por favor.");
+
             }
 
+        }
     }
 }
